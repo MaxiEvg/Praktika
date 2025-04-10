0b0ece8e9272e087cd4f01ba0472655d33165423
Hospin <olegzelin03@gmail.com>
2025-04-10 20:12:41 +0300

    deleted package models

--- admin/database/models/positions.py
+++
@@ -1,9 +0,0 @@
-# positions.py
-from sqlalchemy import Column, Integer, String
-from .base import Base
-
-class Position(Base):
-    __tablename__ = 'position'
-    id = Column(Integer, primary_key=True)
-    title = Column(String, unique=True, nullable=False)
-    description = Column(String)
